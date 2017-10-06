a1dc9696925bbd1ea7246a9221b20fc89c755f4c
JamesMHepworth <jamesmhepworth@hotmail.co.uk>
2017-10-06 11:59:26 +0100

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".navbar-toggle").on("click", function () {
+        $(this).toggleClass("active");
+    });
+});
+
+
+$('.toggle').click(function () {
+    $(this).toggleClass('toggle-open');
+});
+
